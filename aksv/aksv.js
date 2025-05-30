93e680ec685b0cf6ac0b667e8a0f9b10d3147a1a
scigward <162128369+scigward@users.noreply.github.com>
2025-05-30 20:38:15 +0300

    Update aksv.js

--- aksv/aksv.js
+++ aksv/aksv.js
@@ -37,7 +37,7 @@ function extractDetails(html) {
   }
 
   const aliasMatch = html.match(
-    /<div class="font-size-16 text-white mt-2">\s*<span>\s*((مدة (?:المسلسل|الفيلم)\s*:\s*[^<]+))<\/span>\s*<\/div>/
+    /<span>\s*(مدة (?:المسلسل|الفيلم)\s*:\s*[^<]+)<\/span>/i
   );
   if (aliasMatch) {
     aliases = aliasMatch[1].trim();
